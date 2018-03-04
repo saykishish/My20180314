b7470bc18af37b4225ecaba4608f9c14684c5ab8
saykishish <saykishish@gmail.com>
2018-03-04 12:04:31 +0800

    yes20180304

--- app/src/main/java/tw/org/iii/hello/MainActivity.java
+++ app/src/main/java/tw/org/iii/hello/MainActivity.java
@@ -2,12 +2,28 @@ package tw.org.iii.hello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.lottery);
+
     }
+
+    public void test2(View view) {
+        //Log.v("brad","OK");
+
+        textView.setText("Hello, Brad");
+
+
+    }
+
 }
